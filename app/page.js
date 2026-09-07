2ed6cbe6b7e378a1da3f541b59d76f1c70865327
maulana10102020-sys <maulana10102020@gmail.com>
2026-09-07 20:20:07 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Aksanu Exam</h1>
+      <p>Selamat datang. Proyek berhasil dijalankan 🎉</p>
+    </div>
+  );
+}
