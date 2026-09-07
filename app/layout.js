62f8c3a1525d57c8fc00f8f2dc0461f9fa958cde
maulana10102020-sys <maulana10102020@gmail.com>
2026-09-07 20:19:32 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Aksanu Exam',
+  description: 'Sistem ujian online',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
